e8b811481265abd0aabb74e6bf65f689b201ce0f
Dmitry Shvetsov <shvetsovdm@gmail.com>
2022-07-12 18:17:22 +0800

    add ability to pass listener to the app run fn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use newsletter_backend_example::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind a port");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,24 @@
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_test() {
-    spawn_app();
+    let base_url = spawn_app();
     let client = reqwest::Client::new();
 
-    let response = client.get("http://127.0.0.1:8080/health_check").send().await.expect("Request failed");
+    let response = client
+        .get(&format!("{}/health_check", &base_url))
+        .send()
+        .await
+        .expect("Request failed");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = newsletter_backend_example::run().expect("Failed to spawn the app");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind a port");
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter_backend_example::run(listener).expect("Failed to spawn the app");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
